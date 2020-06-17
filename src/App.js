e693f22bf58bb7ea344d167ed6b416b18b8d426e
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-06-17 16:43:44 +0500

    Creating button and updating css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import Switch from "react-switch";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [light1, setLight1] = useState(true);
+  const [light2, setLight2] = useState(true);
   return (
     <div>
-      <h1>Hello World</h1>
+      <div className={`room ${light1 && light2 ? "white" : "black"}`}>
+        <Switch
+          className="switchbutton"
+          checked={light1}
+          onChange={() => setLight1(!light1)}
+          onColor="#86d3ff"
+          onHandleColor="#2693e6"
+          handleDiameter={30}
+          uncheckedIcon={false}
+          checkedIcon={false}
+          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+          height={20}
+          width={48}
+          id="material-switch"
+        />
+      </div>
+      <div className={`room ${light1 && light2 ? "white" : "black"}`}>
+        <Switch
+          className="switchbutton"
+          checked={light2}
+          onChange={() => setLight2(!light2)}
+          onColor="#86d3ff"
+          onHandleColor="#2693e6"
+          handleDiameter={30}
+          uncheckedIcon={false}
+          checkedIcon={false}
+          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+          height={20}
+          width={48}
+          id="material-switch"
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
